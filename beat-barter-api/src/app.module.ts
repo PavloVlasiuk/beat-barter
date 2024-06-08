a010f394623b2dfcb2dd622f8d507afc6721e6fb
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-06-09 01:28:16 +0300

    feat: implement refresh tokens module

--- beat-barter-api/src/app.module.ts
+++ beat-barter-api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { RefreshTokensModule } from './refresh-tokens/refresh-tokens.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, RefreshTokensModule],
 })
 export class AppModule {}
